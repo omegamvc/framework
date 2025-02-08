40b5e64f4b50501ab977a9947c21a1c317a653ff
omegamvc <agisoftt@gmail.com>
2025-02-08 17:11:11 +0100

    Update copyright information

--- src/Application/ApplicationInterface.php
+++ src/Application/ApplicationInterface.php
@@ -2,11 +2,11 @@
 
 /**
  * Part of Omega - Application Package
- * php version 8.2
+ * php version 8.3
  *
  * @link      https://omegamvc.github.io
  * @author    Adriano Giovannini <agisoftt@gmail.com>
- * @copyright Copyright (c) 2024 Adriano Giovannini. (https://omegamvc.github.io)
+ * @copyright Copyright (c) 2024 - 2025 Adriano Giovannini (https://omegamvc.github.io)
  * @license   https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  * @version   1.0.0
  */
@@ -22,7 +22,7 @@ namespace Omega\Application;
  * @package   Application
  * @link      https://omegamvc.github.io
  * @author    Adriano Giovannini <agisoftt@gmail.com>
- * @copyright Copyright (c) 2024 Adriano Giovannini. (https://omegamvc.github.io)
+ * @copyright Copyright (c) 2024 - 2025 Adriano Giovannini (https://omegamvc.github.io)
  * @license   https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  * @version   1.0.0
  */
--- src/Container/ContainerInterface.php
+++ src/Container/ContainerInterface.php
@@ -6,7 +6,7 @@
  *
  * @link      https://omegamvc.github.io
  * @author    Adriano Giovannini <agisoftt@gmail.com>
- * @copyright Copyright (c) 2024 Adriano Giovannini. (https://omegamvc.github.io)
+ * @copyright Copyright (c) 2024 - 2025 Adriano Giovannini (https://omegamvc.github.io)
  * @license   https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  * @version   1.0.0
  */
@@ -38,7 +38,7 @@ use Psr\Container\ContainerInterface as PsrContainerInterface;
  * @package   Container
  * @link      https://omegamvc.github.io
  * @author    Adriano Giovannini <agisoftt@gmail.com>
- * @copyright Copyright (c) 2024 Adriano Giovannini. (https://omegamvc.github.io)
+ * @copyright Copyright (c) 2024 - 2025 Adriano Giovannini (https://omegamvc.github.io)
  * @license   https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  * @version   1.0.0
  */
